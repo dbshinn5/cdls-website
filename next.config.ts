59fc13dbc1c42b1f9804aaf32dfe50933b8e700b
David Shinn <davidshinn@Mac.socal.rr.com>
2026-04-05 20:41:33 -0700

    Bypass Vercel image optimization, use Sanity CDN directly

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ioes.ucla.edu',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ioes.ucla.edu.test',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.ioes.ucla.edu',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-      {
-        protocol: 'http',
-        hostname: 'ioes.ucla.edu.test',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
